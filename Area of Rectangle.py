9c928946bfb0995bd8bf95bc5e72675a0e84a8db
megala79674-sketch <megala79674@gmail.com>
2026-04-29 12:33:19 +0530

    Add files via upload

---
+++ Area of Rectangle.py
@@ -0,0 +1,6 @@
+#area of rectangle
+length=float(input("Enter the length:"))
+breadth=float(input("Enter the breath:"))
+#Enter the area of ractangle
+area=length*breadth
+print("Area of rectangle:",area)
